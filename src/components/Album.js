80a2185e3c46977d2bec4c67b89624a41d867ff0
nahome <nahom.teshome@yahoo.com>
2019-01-24 20:01:55 -0800

    buttons assignment complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -54,6 +54,15 @@ class Album extends Component {
       this.play();
     }
 
+    handleNextClick() {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const last = this.state.album.songs.length - 1;
+      const newIndex = Math.min(last, currentIndex + 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play();
+    }
+
    render() {
      return (
        <section className="album">
@@ -101,6 +110,7 @@ class Album extends Component {
            currentSong={this.state.currentSong}
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
            handlePrevClick={() => this.handlePrevClick()}
+           handleNextClick={() => this.handleNextClick()}
          />
        </section>
      );
--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -11,7 +11,7 @@ import React, { Component } from 'react';
            <button id="play-pause" onClick={this.props.handleSongClick} >
              <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
            </button>
-           <button id="next">
+           <button id="next" onClick={this.props.handleNextClick}>
              <span className="ion-skip-forward"></span>
            </button>
          </section>
